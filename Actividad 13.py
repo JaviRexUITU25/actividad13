16c3b2a5a5c4f78d0b78c420eea2830b4507c598
André <javiandre2007@gmail.com>
2025-08-03 16:35:11 -0600

    primer commit, agregando diccionarios y while true:

---
+++ Actividad 13.py
@@ -0,0 +1,13 @@
+students = {}
+notes = {}
+while True:
+    print("-"*8 + "BIENVENIDO A GESTION ACADEMICA:" + "- *8\n"
+          "1. Agregar estudiante:\n"
+          "2. Agregar curso con nota:  \n"
+          "3. Consultar estudiante: \n"
+          "4. Calcular promedio de estudiante: \n"
+          "5. Verificar estudiante aprobado: \n"
+          "6. Mostrar lista de estudiantes: \n"
+          "7. Salir")
+    option = int(input("Ingrese la opcion que desea ingresar: "))
+
