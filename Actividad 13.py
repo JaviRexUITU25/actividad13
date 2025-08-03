2d84a1f8c86007a2042ebf513c38b973ce75e817
André <javiandre2007@gmail.com>
2025-08-03 16:46:01 -0600

    opcion 1

--- Actividad 13.py
+++ Actividad 13.py
@@ -1,7 +1,7 @@
 students = {}
-notes = {}
+curses = {}
 while True:
-    print("-"*8 + "BIENVENIDO A GESTION ACADEMICA:" + "- *8\n"
+    print("-"*8 + "BIENVENIDO A GESTION ACADEMICA:" + "-" *8 + "\n"
           "1. Agregar estudiante:\n"
           "2. Agregar curso con nota:  \n"
           "3. Consultar estudiante: \n"
@@ -10,4 +10,13 @@ while True:
           "6. Mostrar lista de estudiantes: \n"
           "7. Salir")
     option = int(input("Ingrese la opcion que desea ingresar: "))
+    if option == 1:
+       print("-" * 8 + "AGREGAR ESTUDIANTE"+ "-"*8)
+       amount = int(input("Ingrese la cantidad de estudiantes que desea ingresar: "))
+       for i in range(amount):
+           print(f"Estudiante #{i + 1}")
+           ID = input("Ingrese el ID del estudiante: ")
+           full_name = input("Ingrese el nombre del estudiante: ")
+           major = input("Ingrese la carrera o programa academico del estudiante: ")
+           print("¡Estudiante agregado correctamente!")
 
