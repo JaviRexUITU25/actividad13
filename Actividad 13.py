fa16d66feba3f272b34a570f4cd206ac7bde3254
André <javiandre2007@gmail.com>
2025-08-03 16:58:32 -0600

    opcion 2 y parte de la 1

--- Actividad 13.py
+++ Actividad 13.py
@@ -1,7 +1,7 @@
 students = {}
 curses = {}
-while True:
-    print("-"*8 + "BIENVENIDO A GESTION ACADEMICA:" + "-" *8 + "\n"
+def hello():
+    print("-" * 8 + "BIENVENIDO A GESTION ACADEMICA:" + "-" * 8 + "\n"
           "1. Agregar estudiante:\n"
           "2. Agregar curso con nota:  \n"
           "3. Consultar estudiante: \n"
@@ -9,14 +9,29 @@ while True:
           "5. Verificar estudiante aprobado: \n"
           "6. Mostrar lista de estudiantes: \n"
           "7. Salir")
+
+while True:
+    hello()
     option = int(input("Ingrese la opcion que desea ingresar: "))
-    if option == 1:
-       print("-" * 8 + "AGREGAR ESTUDIANTE"+ "-"*8)
-       amount = int(input("Ingrese la cantidad de estudiantes que desea ingresar: "))
-       for i in range(amount):
+    try:
+        if option == 1:
+           print("-" * 10 + "AGREGAR ESTUDIANTE"+ "-"* 10)
+           amount = int(input("Ingrese la cantidad de estudiantes que desea ingresar: "))
+    except ValueError:
+        print("Ingrese un numero valido, intente de nuevo.")
+        for i in range(amount):
            print(f"Estudiante #{i + 1}")
            ID = input("Ingrese el ID del estudiante: ")
            full_name = input("Ingrese el nombre del estudiante: ")
            major = input("Ingrese la carrera o programa academico del estudiante: ")
            print("¡Estudiante agregado correctamente!")
+        if option == 2:
+           print("-"*10 + "AGREGAR CURSOS Y NOTA"+ "-"* 10)
+           buscado = input("Ingrese el ID del estudiante: ")
+           if buscado in students:
+               curse_name = input("Ingrese el nombre del curso: ")
+               final_note = input("Ingrese la nota del curso: ")
+           else:
+                print("Ingrese un ID valido, intente de nuevo.")
+
 
