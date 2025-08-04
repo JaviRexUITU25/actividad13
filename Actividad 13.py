3c8e8fb41c4e4fda3c0413376198d3418a9c405d
Javier Huertas <javiandre2007@gmail.com>
2025-08-04 13:02:20 -0600

    arreglando opcion 1, usando funciones

--- Actividad 13.py
+++ Actividad 13.py
@@ -1,35 +1,35 @@
-from unittest import case
-
 students = {}
 courses = {}
 def hello():
     print("-" * 8 + "BIENVENIDO A GESTION ACADEMICA:" + "-" * 8 + "\n"
-          "1. Agregar estudiante:\n"
-          "2. Agregar curso con nota:  \n"
-          "3. Consultar estudiante: \n"
-          "4. Calcular promedio de estudiante: \n"
-          "5. Verificar estudiante aprobado: \n"
-          "6. Mostrar lista de estudiantes: \n"
-          "7. Salir")
+          "1. Agregar estudiante y agregar curso con nota:\n"
+          "2. Consultar estudiante: \n"
+          "3. Calcular promedio de estudiante: \n"
+          "4. Verificar estudiante aprobado: \n"
+          "5. Mostrar lista de estudiantes: \n"
+          "6. Salir")
+def option1():
+    print("-" * 10 + "AGREGAR ESTUDIANTE" + "-" * 10)
+    amount = int(input("Ingrese la cantidad de estudiantes que desea ingresar: "))
+    for x in range(amount):
+        print(f"Estudiante #{i + 1}")
+        ID = input("Ingrese el ID del estudiante: ")
+        full_name = input("Ingrese el nombre del estudiante: ")
+        major = input("Ingrese la carrera o programa academico del estudiante: ")
+        print("¡Estudiante agregado correctamente!")
+        students[ID] = {
+            "ID" : ID,
+            "Nombre" : full_name,
+            "Carrera" : major
+        }
+
 while True:
     hello()
     option = int(input("Ingrese la opcion que desea ingresar: "))
     try:
         match option:
             case 1:
-               print("-" * 10 + "AGREGAR ESTUDIANTE"+ "-"* 10)
-               amount = int(input("Ingrese la cantidad de estudiantes que desea ingresar: "))
-               for i in range(amount):
-                   print(f"Estudiante #{i + 1}")
-                   ID = input("Ingrese el ID del estudiante: ")
-                   full_name = input("Ingrese el nombre del estudiante: ")
-                   major = input("Ingrese la carrera o programa academico del estudiante: ")
-                   print("¡Estudiante agregado correctamente!")
-                   students['ID'] = {
-                       'ID': ID,
-                       'full_name': full_name,
-                       'major': major
-                   }}
+                option1()
     except ValueError:
         print("Ingrese un numero valido, intente de nuevo.")
     except Exception as e:
