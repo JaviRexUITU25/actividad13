a0e8588c8526b45d257ad84eceee243b3c9ae8fe
André <javiandre2007@gmail.com>
2025-08-04 21:07:56 -0600

    ya funcionan las opciones 1,2 quedan lo demas jiji

--- Actividad 13.py
+++ Actividad 13.py
@@ -1,15 +1,14 @@
 students = {}
 courses = {}
-def hello():
+def hello(): #SALUDO GENERAL
     print("-" * 8 + "BIENVENIDO A GESTION ACADEMICA:" + "-" * 8 + "\n"
           "1. Agregar estudiante.\n"
-          "2. Agregar curso con nota.\n"
-          "3. Consultar estudiante. \n"
-          "4. Calcular promedio de estudiante. \n"
-          "5. Verificar estudiante aprobado. \n"
-          "6. Mostrar lista de estudiantes. \n"
-          "7. Salir.")
-def option1():
+          "2. Consultar estudiante. \n"
+          "3. Calcular promedio de estudiante. \n"
+          "4. Verificar estudiante aprobado. \n"
+          "5. Mostrar lista de estudiantes. \n"
+          "6. Salir.")
+def option1(): #SI EL USUARIO QUIERE AGREGAR UN ESTUDIANTE
     print("-" * 10 + "AGREGAR ESTUDIANTE" + "-" * 10)
     try:
         amount = int(input("Ingrese la cantidad de estudiantes que desea ingresar: "))
@@ -18,29 +17,30 @@ def option1():
             idd = input("Ingrese el ID del estudiante: ").upper()
             full_name = input("Ingrese el nombre del estudiante: ")
             major = input("Ingrese la carrera o programa academico del estudiante: ")
+            students[idd] = {
+                "full_name": full_name,
+                "major": major
+            }
             print("-"*10 + "¡Estudiante agregado correctamente!"+ "-"*10)
-            option2 =input("1. Si\n"
+            option2 =input("1. Si\n"                                        #SI EL USUARIO QUIERE AGREGAR EL CURSO Y SU NOTA
                            "2. No\n"
                            "¿Desea agregar un curso y su nota?: ")
             try:
                 if option2 == "1":
                     search_id = int(input("Ingrese el ID del estudiante: "))
-                    if idd not in students:
+                    if idd not in students: #por si el estudiante no ha sido registrado
                         print("El estudiante no existe")
                     else:
-                        course_a = int(input("Ingrese cuantos cursos desea implementar: "))
+                        course_a = int(input("Ingrese cuantos cursos desea implementar: ")) #Verificar el numero de cursos
                         for y in range(course_a):
                             print(f"curso #{y + 1}")
                             course_name = input("Ingrese nombre de curso: ")
                             course_note = input("Ingrese nota de curso: ")
                             print("-" * 10 + "¡Curso agregado correctamente!" + "-" * 10)
-                            print("Lista de estudiantes:")
-                            for idd, example in students.items():
-                                print(f"ID: {idd}\n"
-                                      f"Nombre: {example['full_name']}\n"
-                                      f"Carrera: {example['major']}\n"
-                                      f"Curso: {example['course']}\n"
-                                      f"Nota: {example['course']}\n")
+                            courses[course_name] = {
+                                "course_name": course_name,
+                                "course_note": course_note
+                            }
                 elif option2 == "2":
                     continue
             except ValueError:
@@ -55,29 +55,34 @@ def option1():
         print("El valor ingresado no es valido.")
     except Exception as e:
         print("Un error inesperado ha ocurrido")
-'''def option2():
+def option2():
     try:
-    except ValueError:
-            print("El valor ingresado no es valido.")
-    except TypeError:
-            print("El valor ingresado no es valido.")
-    except Exception as e:
-            print("Un error inesperado ha ocurrido")
-def student_search():
-    search_id2 = input("Ingrese el ID del estudiante: ")
-    if search_id2 not in students:
-        print("El estudiante no existe")
-    else:
-        print("=" * 10 + "CONSULTAR ESTUDIANTE" + "="*10)
-        searching = input("Ingrese ID del estudiante: ")
-        if searching in students:
-            student = students[searching]
-            print("Estudiante encontrado!\n"
-                  f"Nombre: {student["full_name"]}\n"
-                  f"Carrera: {student["major"]}\n"
-                  f"CUrso y nota: {student["Curso"],["Nota de curso"]}")
+        print("=" * 10 + "CONSULTAR ESTUDIANTE" + "=" * 10)
+        search_id2 = input("Ingrese el ID del estudiante: ").upper()
+        if search_id2 not in students:
+            print("El estudiante no existe")
         else:
-            print("Estudiante no encontrado")'''
+            for id2,values in students.items():
+                if id2 == search_id2:
+                    print(f"ID: {id2}\n"
+                          f"Nombre del el estudiante: {values['full_name']}\n"
+                          f"Carrera: {values['major']}\n")
+                    if not courses:
+                        print("Sin cursos asignados")
+                    else:
+                        for id3, values2 in courses.items():
+                            if id3 == search_id2:
+                                print(f"Nombre del curso: {values2['course_name']}\n"
+                                      f"Nota del curso: {values2['course_note']}\n")
+    except ValueError:
+        print("El valor ingresado no es valido.")
+    except TypeError:
+        print("El valor ingresado no es valido.")
+    except Exception as e:
+        print("Un error inesperado ha ocurrido")
+def average():
+
+
 while True:
     hello()
     option = int(input("Ingrese la opcion que desea ingresar: "))
@@ -85,6 +90,8 @@ while True:
         match option:
             case 1:
                 option1()
+            case 2:
+                option2()
     except ValueError:
         print("El valor ingresado no es valido.")
     except TypeError:
